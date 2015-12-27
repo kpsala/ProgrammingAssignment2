558282349fd8c21260e3375efcd623ebd4df0437
Kyle Sala <kyle.paul.sala@gmail.com>
2015-12-27 12:18:14 -0500

    updated to include latest saves

--- cachematrix.R
+++ cachematrix.R
@@ -1,32 +1,44 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This function creates a special object that contains a list of
+## functions and values to be called by cacheSolve to compute a 
+## matrix inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-    mi <- NULL
-    set <- function(y){
-        x <<- y
-        mi <<- NULL
-    }
-    get <- function() x
+    mi <- NULL #assign the matrix inverse to null within this function
     
-    m <- NULL
-    set <- function(y) {
-        x <<- y
-        m <<- NULL
-    }
-    get <- function() x
-    setmean <- function(mean) m <<- mean
-    getmean <- function() m
-    list(set = set, get = get,
-         setmean = setmean,
-         getmean = getmean)
+    get <- function() x #x is the input to makeCacheMatrix
+    
+    #set the inverse, mi, globally using the "<<-" operator
+    setinverse <- function(inverse) mi <<- inverse
+    
+    getinverse <- function() mi #return the value of mi
+    
+    #store all functions in a list
+    list(get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function takes the output of makeCahceMatrix and first 
+## checks to see if the matrix inverse exists, if it does not, it 
+## computes it
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    
+    mi <- x$getinverse()
+    if(!is.null(mi)){ #mi exists, return the inverse
+        message("getting cached data")
+        return(mi)
+    }
+    
+    # if the inverse exists, the function will exit on "return
+    # compute the inverse, and store it with the "setinverse" function
+    
+    mi <- solve(x$get())
+    x$setinverse(mi)
+    mi
 }
